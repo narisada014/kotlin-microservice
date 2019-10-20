62660e7a4fd1c0802602a34d6b01dedd161c7621
narisada014 <nari.reishi@gmail.com>
2019-10-20 22:49:49 +0900

    add post request

--- demo2/src/main/kotlin/com/microservice/microdemo2/CustomerController.kt
+++ demo2/src/main/kotlin/com/microservice/microdemo2/CustomerController.kt
@@ -1,12 +1,28 @@
 package com.microservice.microdemo2
 
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.ResponseBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.*
+import java.util.concurrent.ConcurrentHashMap
 
 @RestController
 class CustomerController {
-    @RequestMapping(value = "/customer", method = arrayOf(RequestMethod.GET))
-    fun getCustomer() = Customer(1, "Kotlin")
+    @Autowired
+    // lateinitは変数の遅延評価。メモリ省エネ用（呼び出されるまで使わなくていいものだから）
+    // ConcurrentHashMapは引数の型でdataを配列内にハッシュで格納する。
+    lateinit var customers: ConcurrentHashMap<Int, Customer>
+
+    @RequestMapping(value = "/customer/{id}", method = arrayOf(RequestMethod.GET))
+    fun getCustomer(@PathVariable id: Int) = customers[id]
+
+    @RequestMapping(value = "/customers", method = arrayOf(RequestMethod.GET))
+    // nameFileterに指定した文字列のデータのみをフィルタリングして返すようにしている。
+    fun getCustomers(@RequestParam(required = false, defaultValue = "") nameFilter: String) =
+            customers.filter {
+                it.value.name.contains(nameFilter, true)
+            }.map(Map.Entry<Int, Customer>::value).toList()
+
+    @RequestMapping(value = "/customer/", method = arrayOf(RequestMethod.POST))
+    fun createCustomer(@RequestBody customer: Customer) {
+        customers[customer.id] = customer
+    }
 }
--- demo2/src/main/kotlin/com/microservice/microdemo2/Microdemo2Application.kt
+++ demo2/src/main/kotlin/com/microservice/microdemo2/Microdemo2Application.kt
@@ -2,9 +2,27 @@ package com.microservice.microdemo2
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import java.util.concurrent.ConcurrentHashMap
 
 @SpringBootApplication
-class Microdemo2Application
+class Microdemo2Application {
+	// Javaのstatic methodと同様な使い方ができる
+	// Javaでstaticメソッドを作る場合はRubyでいうクラスメソッド的な位置付けになる。
+	// いちいちインスタンスを作成し、メモリを利用する必要がないものはstaticで呼び出せばいい
+	// また、staticメソッドを呼ぶものはstatic オブジェクトでなければならず型的な使い方もできる。
+	companion object {
+	    val initialCustomers = arrayOf(Customer(1, "Kotlin"),
+				Customer(2, "Spring"),
+				Customer(3, "Microservice"))
+	}
+
+	@Bean
+	// initialCustomerのオブジェクトをCustomerオブジェクトのidで結びつける。
+	// ConcurrentHashMap<Int, String>()を行うと{}が生成される。()内の引数はarrayOfで定義されたオブジェクト
+	// Customerのidとハッシュ配列のインデックスIdが紐づけられる => {1=Customer(id=1, name=kotlin)} こんな感じ
+	fun customers() = ConcurrentHashMap<Int, Customer>(initialCustomers.associateBy(Customer::id))
+}
 
 fun main(args: Array<String>) {
 	runApplication<Microdemo2Application>(*args)
